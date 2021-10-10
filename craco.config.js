e266b9536e0d64f24ef498a7200f641c7c82566f
Antonio Ochoa <antonhub00@gmail.com>
2021-10-10 18:16:38 -0500

    Tailwind added to the project

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// Craco needed to override PostCSS for "create react app"
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
