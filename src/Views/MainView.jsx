848065ab01d7390de56b2512ffc7e054cad0a283
Antonio Ochoa <antonhub00@gmail.com>
2021-10-11 18:37:21 -0500

    Card component added

--- src/Views/MainView.jsx
+++ src/Views/MainView.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Section from "../components/Section";
+import Card from "../components/Card";
 
 const MainView = () => (
   <div className="md:container m-auto">
     <NavBar />
     <Section />
+    <Card />
   </div>
 );
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import imageNotFound from "../images/imageNotFound.png";
+
+const Card = ({ imgURL, title, content }) => (
+  <div className="rounded-sm bg-white overflow-hidden max-w-xs shadow-md">
+    <div
+      style={{
+        backgroundImage: `url(${imgURL || imageNotFound})`,
+      }}
+      className="h-40 max-w-sm bg-cover bg-center"
+    />
+    <div className="mx-6 my-4 ">
+      <div className="font-bold text-xl text-gray-600">
+        {title || "No title"}
+      </div>
+      <p className="mt-2 text-gray-500">{content || "No content"}</p>
+    </div>
+  </div>
+);
+export default Card;
