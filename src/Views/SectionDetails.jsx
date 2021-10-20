244e7f1eea2e5079b0e1b614296df890e1555940
Antonio Ochoa <antonhub00@gmail.com>
2021-10-20 02:14:57 -0500

    Section details view implemented

--- src/Views/SectionDetails.jsx
+++ src/Views/SectionDetails.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Card from "../components/Card";
+import data from "../api/mock.json";
 
-const SectionDetails = ({ match }) => <div>id: {match.params.id}</div>;
+const SectionDetails = () => {
+  const { id } = useParams();
+  const [sectionItems, setSectionItems] = useState([]);
+
+  const getSectionItemsById = () =>
+    data.data.find((section) => section.sectionId === Number(id)).sectionItems;
+
+  useEffect(() => {
+    setSectionItems(getSectionItemsById());
+  }, []);
+
+  return (
+    <div className="p-6 flex flex-wrap gap-4 justify-center items-center min-h-screen">
+      {sectionItems.map(
+        ({
+          sectionItemId,
+          sectionItemTitle,
+          sectionItemContent,
+          sectionItemImage,
+        }) => (
+          <Card
+            key={sectionItemId}
+            title={sectionItemTitle}
+            content={sectionItemContent}
+            imgURL={sectionItemImage}
+          />
+        )
+      )}
+    </div>
+  );
+};
 
 export default SectionDetails;
