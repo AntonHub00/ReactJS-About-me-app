e1e86f48f17a855f2d9c07f1bc36a4f5ab837aa7
Antonio Ochoa <antonhub00@gmail.com>
2021-10-19 19:20:48 -0500

    Main route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import MainView from "./Views/MainView";
 
 const App = () => (
-  <>
-    <MainView />
-  </>
+  <BrowserRouter>
+    <Route path="/" exact render={MainView} />
+  </BrowserRouter>
 );
 export default App;
