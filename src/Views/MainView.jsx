7d58e95413d177deb82fbc530095666a78dc125b
Antonio Ochoa <antonhub00@gmail.com>
2021-10-19 19:08:14 -0500

    Section display in Main view with random colors

--- src/Views/MainView.jsx
+++ src/Views/MainView.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import Section from "../components/Section";
-import Card from "../components/Card";
+import Section, { baseColors } from "../components/Section";
+import data from "../api/mock.json";
 
-const MainView = () => (
-  <div className="md:container m-auto">
-    <NavBar />
-    <Section />
-    <Card />
-  </div>
-);
+const MainView = () => {
+  const chooseRandomColor = () => {
+    const colorsArray = Object.keys(baseColors);
+    return colorsArray[Math.floor(Math.random() * colorsArray.length)];
+  };
+
+  return (
+    <div className="md:container m-auto">
+      <NavBar />
+      <div className="py-6 flex flex-row gap-4 flex-wrap">
+        {data.data.map((section) => (
+          <Section
+            key={section.sectionId}
+            title={section.sectionTitle}
+            content={section.sectionContent}
+            color={chooseRandomColor()}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MainView;
